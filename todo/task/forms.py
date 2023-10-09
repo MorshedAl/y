95ae2a07af6777bea37a83e67966643c97db3c0d
MorshedAl <moshedabdullah@gmail.com>
2023-10-09 08:43:15 +0600

    first commit

---
+++ todo/task/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from task.models import TaskModel
+
+
+class TaskModalForm(forms.ModelForm):
+    class Meta:
+        model = TaskModel
+        exclude = ['is_completed']
